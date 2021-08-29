201c2524c3dc8663fce8575cb7860abea34ed94f
stevensetiawan94 <steven.setiawan@nusantara.technology>
2021-08-29 22:50:22 +0700

    update script

--- config/database.js
+++ config/database.js
@@ -22,16 +22,13 @@ module.exports = {
       updatedAt: 'updated_at'
     }
   },
-  production: {    
-    username: "b8bd5aadbe644c",
-    password: "17e14fe0",
-    database: `kanggo`,
-    host: "us-cdbr-east-04.cleardb.com",
-    dialect: "mysql",
-    logging: false,
-    define:{
-      createdAt: 'created_at',
-      updatedAt: 'updated_at'
+  production: {
+    use_env_variable: 'mysql://b8bd5aadbe644c:17e14fe0@us-cdbr-east-04.cleardb.com/heroku_e7fd9b8bb7c853b?reconnect=true',
+    dialectOptions: {
+      ssl: {
+          require: true,
+          rejectUnauthorized: false
+      }
     }
   }
 }
